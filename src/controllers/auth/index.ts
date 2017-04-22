a145a911e2d33fc8adf7a6f59b11c1e347afb06e
garytryan <gary.thomas.ryan@gmail.com>
2017-04-21 21:57:33 -0700

    redirecting to google

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -67,8 +67,7 @@ export default {
 
     const accessToken = oauthRequestTokenResponse.match(/oauth_token=[a-z|A-Z|0-9]+/)[0].replace('oauth_token=', '')
 
-    ctx.response.redirect(`https://www.google.com`)
-    // ctx.response.redirect(`https://api.twitter.com/oauth/authenticate?oauth_token=${accessToken}`)
+    ctx.response.redirect('http://www.google.com')
   },
 
   twitterCallback: async ctx => {
